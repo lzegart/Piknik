3ae29fb293147b3bc9dc2f1edbaaadb4230d905c
Shellsea <elainesbarrera@gmail.com>
2020-11-09 15:04:17 -0800

    UPDATE: added comments for map code

--- assets/script.js
+++ assets/script.js
@@ -32,17 +32,21 @@ console.log("Hey guys!")
 
 // Shelsy
 
-let myToken = 'sk.eyJ1Ijoic2hlbGxzZWEzMSIsImEiOiJja2hiMzBuemsxOHczMnNrODF6M2lveHc1In0.WXY65kiQoG9rDXMxzd5qEg'
+// experimenting with the map created using mapbox.js
 L.mapbox.accessToken = `pk.eyJ1Ijoic2hlbGxzZWEzMSIsImEiOiJja2hiMnVsdzUwbThsMndrNDUyNnI0dDJuIn0.Fwya7JTKf9MQOsTVGMVwIg`
 let map = L.mapbox.map('map')
 .setView([40, -74.50], 9)
 .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v11'));
 
-// fetch (`https://api.mapbox.com/geocoding/v5/mapbox.places/${currentLocation}.json?access_token=sk.eyJ1Ijoic2hlbGxzZWEzMSIsImEiOiJja2hiMzBuemsxOHczMnNrODF6M2lveHc1In0.WXY65kiQoG9rDXMxzd5qEg`)
-// .then(function(response) {
-//     return response.json();
-// })
-// .then (function(data){
-//     console.log(data);
-// })
+// experimenting with the geocoding api from mapbox website
+// PLEASE DO NOT DELETE THIS SECRET KEY 
+let myToken = 'sk.eyJ1Ijoic2hlbGxzZWEzMSIsImEiOiJja2hiMzBuemsxOHczMnNrODF6M2lveHc1In0.WXY65kiQoG9rDXMxzd5qEg'
+let currentLocation = "San Jose, CA"
+fetch (`https://api.mapbox.com/geocoding/v5/mapbox.places/${currentLocation}.json?access_token=${myToken}`)
+.then(function(response) {
+    return response.json();
+})
+.then (function(data){
+    console.log(data);
+})
 
