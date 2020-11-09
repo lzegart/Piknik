c9a2aca83afb343cf6d4e23f95e03171ceb6532c
Shellsea <elainesbarrera@gmail.com>
2020-11-09 14:56:29 -0800

    UPDATE: working map using mapbox.js

--- assets/script.js
+++ assets/script.js
@@ -31,3 +31,18 @@ console.log("Hey guys!")
 
 
 // Shelsy
+
+let myToken = 'sk.eyJ1Ijoic2hlbGxzZWEzMSIsImEiOiJja2hiMzBuemsxOHczMnNrODF6M2lveHc1In0.WXY65kiQoG9rDXMxzd5qEg'
+L.mapbox.accessToken = `pk.eyJ1Ijoic2hlbGxzZWEzMSIsImEiOiJja2hiMnVsdzUwbThsMndrNDUyNnI0dDJuIn0.Fwya7JTKf9MQOsTVGMVwIg`
+let map = L.mapbox.map('map')
+.setView([40, -74.50], 9)
+.addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v11'));
+
+// fetch (`https://api.mapbox.com/geocoding/v5/mapbox.places/${currentLocation}.json?access_token=sk.eyJ1Ijoic2hlbGxzZWEzMSIsImEiOiJja2hiMzBuemsxOHczMnNrODF6M2lveHc1In0.WXY65kiQoG9rDXMxzd5qEg`)
+// .then(function(response) {
+//     return response.json();
+// })
+// .then (function(data){
+//     console.log(data);
+// })
+
