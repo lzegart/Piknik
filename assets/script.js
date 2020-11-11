e2b90a7d39a13228d62d71d477d07aefca330f5c
Shellsea <elainesbarrera@gmail.com>
2020-11-11 12:17:12 -0800

    UPDATE: saved users form selections

--- assets/script.js
+++ assets/script.js
@@ -6,61 +6,79 @@ console.log("Hey guys!");
 
 // Alfredo
 
+// gives modals functionality
 $(document).foundation();
 
 // Shelsy
 
+// target user address
+let input = document.querySelector("input");
+// target user miles
+let mileInput = document.querySelector("#miles");
+// target user food preference
+let foodInput = document.querySelector("#foods");
+// target user place preference
+let placeInput = document.querySelector("#places");
 
-// experiment with different addresses. Very case sensitive.
-// entering an address will render its geographic coords (AKA forward geocoding)
-// entering coords will render its varying address (AKA reverse geocoding)
-let currentLocation = "633 Sinclair Dr, San Jose, CA";
+// when user clicks "plan my piknik"
+document.querySelector("#planPiknik").addEventListener("click", function(e){
+  e.preventDefault();
+  // get values of all options they chose
+  let currentLocation = input.value;
+  console.log(currentLocation);
+  let miles = mileInput.value;
+  console.log(miles);
+  let food = foodInput.value;
+  console.log(food);
+  let place = placeInput.value;
+  console.log(place);
+});
 
 // experimenting with the map created using mapbox.js
-L.mapbox.accessToken = `pk.eyJ1Ijoic2hlbGxzZWEzMSIsImEiOiJja2hiMnVsdzUwbThsMndrNDUyNnI0dDJuIn0.Fwya7JTKf9MQOsTVGMVwIg`
+// L.mapbox.accessToken = `pk.eyJ1Ijoic2hlbGxzZWEzMSIsImEiOiJja2hiMnVsdzUwbThsMndrNDUyNnI0dDJuIn0.Fwya7JTKf9MQOsTVGMVwIg`
 
-let geocoder = L.mapbox.geocoder('mapbox.places');
+// let geocoder = L.mapbox.geocoder('mapbox.places');
 
-let map = L.mapbox.map('mapBox').addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v11'));
+// let map = L.mapbox.map('mapBox').addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v11'));
 
-geocoder.query(`${currentLocation}`, showMap);
+// geocoder.query(`${currentLocation}`, showMap);
 
-function showMap(err, data) {
-    // The geocoder can return an area, like a city, or a
-    // point, like an address. Here we handle both cases,
-    // by fitting the map bounds to an area or zooming to a point.
-    if (data.lbounds) {
-        map.fitBounds(data.lbounds);
-    } else if (data.latlng) {
-        map.setView([data.latlng[0], data.latlng[1]], 13);
-    }
-}
+// function showMap(err, data) {
+//     // The geocoder can return an area, like a city, or a
+//     // point, like an address. Here we handle both cases,
+//     // by fitting the map bounds to an area or zooming to a point.
+//     if (data.lbounds) {
+//         map.fitBounds(data.lbounds);
+//     } else if (data.latlng) {
+//         map.setView([data.latlng[0], data.latlng[1]], 13);
+//     }
+// }
 
 // experimenting with the geocoding api from mapbox website
 // PLEASE DO NOT DELETE THIS SECRET KEY
-let myToken =
-  "sk.eyJ1Ijoic2hlbGxzZWEzMSIsImEiOiJja2hiMzBuemsxOHczMnNrODF6M2lveHc1In0.WXY65kiQoG9rDXMxzd5qEg";
+// let myToken =
+//   "sk.eyJ1Ijoic2hlbGxzZWEzMSIsImEiOiJja2hiMzBuemsxOHczMnNrODF6M2lveHc1In0.WXY65kiQoG9rDXMxzd5qEg";
 
-function getCoords(){
-  fetch(
-    `https://api.mapbox.com/geocoding/v5/mapbox.places/${currentLocation}.json?access_token=${myToken}`
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
+// function getCoords(){
+//   fetch(
+//     `https://api.mapbox.com/geocoding/v5/mapbox.places/${currentLocation}.json?access_token=${myToken}`
+//   )
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
   
-      // set variables for lat and lon of currentLocation
-      let lon = data.features[1].geometry.coordinates[0]
-      let lat = data.features[1].geometry.coordinates[1]
-      console.log(lon);
-      console.log(lat);
-      return lat, lon;
-    });
-}
+//       // set variables for lat and lon of currentLocation
+//       let lon = data.features[1].geometry.coordinates[0]
+//       let lat = data.features[1].geometry.coordinates[1]
+//       console.log(lon);
+//       console.log(lat);
+//       return lat, lon;
+//     });
+// }
 
-getCoords()mmm
+// getCoords()
 
 
 
@@ -80,15 +98,15 @@ getCoords()mmm
 // 5 miles is 8046.72
 
 
-  fetch(
-    `https://api.tomtom.com/search/2/poiSearch/pizza.json?lat=${lat}&lon=${lon}&radius=16093&categorySet=7315&key=yrlw2N38GKc3iSGnqvnQNxPQUsVQuVAh`
-  )
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data){
-    console.log(data)
-  });
+  // fetch(
+  //   `https://api.tomtom.com/search/2/poiSearch/pizza.json?lat=${lat}&lon=${lon}&radius=16093&categorySet=7315&key=yrlw2N38GKc3iSGnqvnQNxPQUsVQuVAh`
+  // )
+  // .then(function(response) {
+  //   return response.json();
+  // })
+  // .then(function(data){
+  //   console.log(data)
+  // });
 
 
 
